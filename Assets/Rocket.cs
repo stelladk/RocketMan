b69f363b388de683e3b40cc3555acb691c99e7d8
Stella <35050451+stelladk@users.noreply.github.com>
2019-12-12 01:19:15 +0200

    Basic Listeners

---
+++ Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput(){
+        if(Input.GetKey(KeyCode.Space)){
+            print("Space");
+            //TODO: go upwards
+        }
+        if(Input.GetKey(KeyCode.A)){
+            print("left");
+            //TODO: turn left
+        }else if(Input.GetKey(KeyCode.D)){
+            print("right");
+            //TODO: turn right
+        }
+    }
+}
