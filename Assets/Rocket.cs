a8b701afb18e16100b3ce8dc77f3c7ee826220fe
Stella <35050451+stelladk@users.noreply.github.com>
2019-12-12 22:19:50 +0200

    Game Controls

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput(){
+    private void Thrust(){
         if(Input.GetKey(KeyCode.Space)){
-            print("Space");
-            //TODO: go upwards
+            rigidBody.AddRelativeForce(Vector3.up);
         }
+    }
+
+    private void Rotate(){
+        rigidBody.freezeRotation = true;
+
         if(Input.GetKey(KeyCode.A)){
-            print("left");
-            //TODO: turn left
+            transform.Rotate(Vector3.forward);
         }else if(Input.GetKey(KeyCode.D)){
-            print("right");
-            //TODO: turn right
+            transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false;
     }
 }
